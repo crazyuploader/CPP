9a50ec8aa0e58e6857c1cae5fda1cad974648612
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2019-10-04 13:33:23 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,17 +5,6 @@ using namespace std;
 #define NEWLINE '\n'
 #define TAB '\t'
 
-int build_time()
-{
-
-	time_t tt;
-	struct tm * ti;
-	time (&tt);
-	ti = localtime(&tt);
-	cout << "Current Day, Date and Time is = "<< asctime(ti);
-	return 0;
-}
-
 int footer()
 {
     cout<< "\n";
