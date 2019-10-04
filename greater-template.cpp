5293fcd044047fd08dbb3b9186f8e976bac5c51f
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2019-10-04 09:11:23 +0530

    Create greater-template.cpp

---
+++ greater-template.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+#define NEWLINE '\n'
+#define TAB '\t'
+
+template<class T>
+T Large(T x, T y)
+{
+    return (x>y)?x:y;
+}
+
+int main()
+{
+    int i1,i2;
+    float f1,f2;
+    char c1,c2;
+    cout<< "///Program to Display Greater Number using Template///"<< endl<< endl<< endl;
+    cout<< "Enter Integers: ";
+    cin>> i1>> i2;
+    cout<< "Enter Floating Point: ";
+    cin>> f1>> f2;
+    cout<< "Enter Character: ";
+    cin>> c1>> c2;
+    cout<< Large(i1,i2)<< " is Larger."<< endl;
+    cout<< Large(f1,f2)<< " is Larger."<< endl;
+    cout<< Large(c1,c2)<< " is Larger."<< endl;
+    cout<< NEWLINE<< NEWLINE<< "Created by Jugal Kishore -- 2019"<< NEWLINE;
+}
