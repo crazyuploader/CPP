2ebccdf4addd745d4f1a997dec6ca538a766ae76
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2019-10-08 14:04:04 +0530

    fun

--- main.cpp
+++ main.cpp
@@ -26,18 +26,10 @@
 #include"subtract_2.h"
 #include"sum_digits.h"
 #include"table.h"
+#include"footer.h"
 using namespace std;
 #define NEWLINE '\n'
 #define TAB '\t'
-
-int footer()
-{
-    cout<< "\n";
-    cout<< TAB<< " =================================\n";
-    cout<< TAB<< "||Created by Jugal Kishore -- 2019||"<< NEWLINE;
-    cout<< TAB<< " =================================\n";
-    return 0;
-}
 
 int main()
 {
@@ -131,6 +123,7 @@ int main()
               break;
      default: cout<< "Exiting...\n";
               footer();
+              name();
               exit(0);
    }
    cout<<"\n";
