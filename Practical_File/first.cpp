b64cf6e8d561c92689b525bb8ad15867b2c4da41
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2019-10-30 17:37:33 +0530

    update
    adding array to first.cpp

--- Practical_File/first.cpp
+++ Practical_File/first.cpp
@@ -1,14 +1,38 @@
-//Program to demonstrate the use of new and delete operator
+//Program to demonstrate the use of
+//new and delete operator
 #include<iostream>
 using namespace std;
 
 int main()
 {
-    //Initialization pointer p to NULL
-    int *p=NULL;
-    p=new int;
-    cout<< "Pointer value is "<< *p;
-    //Freeing pointer p now
-    delete p;
+    //Initialization of pointer ptr to NULL
+    int *ptr=NULL;
+    //Allocating size of 'int' to pointer ptr
+    ptr=new int;
+    cout<< "Enter pointer value: ";
+    cin>> *ptr;
+    cout<< "Pointer value is "<< *ptr;
+    //Initialization of Array
+    int *array=NULL;
+    int i,n;
+    cout<< "\n\nEnter no. of elements in Array: ";
+    cin>> n;
+    //Allocating elements of size 'int'
+    array=new int[n];
+    cout<< "Enter Array:\n";
+    //Entering Elements
+    for(i=0;i<n;i++)
+    {
+        cin>> array[i];
+    }
+    cout<< "Entered Array is:";
+    //Displaying Elements
+    for(i=0;i<n;i++)
+    {
+        cout<< " "<<array[i];
+    }
+    //Freeing memory
+    delete ptr;
+    delete array;
     return 0;
 }
