4aa061ec1b2e2507db1549c58839b74c28774b6f
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2019-12-07 15:31:33 +0530

    adding build.sh
    Just another Build Script

--- trick.cpp
+++ trick.cpp
@@ -25,22 +25,38 @@ int main()
     cin>> name;
     cout<< "\nHi "<< name<< "!"<< endl<< endl;
     cout<< "Let's Play?"<< endl<< endl;
-    cout<< "Press 1 to Continue or 2 to Exit"<< endl;
+    cout<< "Press 1 to Continue or anything else to Exit"<< endl;
+    cin>> temp;
+    if(temp!=1)
+    {
+        cout<< "\nOff my program you "<< name<< " go!\nExiting...";
+        cout<< NEWLINE<< NEWLINE<< "Created by Jugal Kishore -- 2019"<< NEWLINE;
+        exit(0);
+    }
+    int secret=gen();
+    cout<< "\nOkay, here it is\nJust do as I say - \n\n";
+    cout<< "Step 1. Think of a Number.\n";
+    cout<< "Step 2. Double it.\n";
+    cout<< "Step 3. Add --->> "<< secret<< " in it.\n";
+    cout<< "Step 4. Now half the Number.\n";
+    cout<< "Step 5. Subtract the Original Number you had thought of.\n\n";
+    cout<< "Final Answer is: --->> "<< secret/2<< endl;
+    cout<< "\nPress 1 to see how this trick works or 2 to Exit"<< endl;
     cin>> temp;
     if(temp==2)
     {
-        cout<< "\nOff my program you go!\nExiting...";
+        cout<< "\nOff my program you "<< name<< " go!\nExiting...";
+        cout<< NEWLINE<< NEWLINE<< "Created by Jugal Kishore -- 2019"<< NEWLINE;
         exit(0);
     }
-    int secret = gen();
-    cout<< "\nOkay, here it is\nJust do as I say - \n\n";
-    cout<< "1. Think of a Number.\n";
-    cout<< "2. Double it.\n";
-    cout<< "3. Add --->> "<< secret<< " in it.\n";
-    cout<< "4. Now half the number.\n";
-    cout<< "5. Subtract the original number you had thought of.\n\n";
-    cout<< "Final Answer is: --->> "<< secret/2;
-    cout<< "\nBye Bye "<< name;
+    cout<< "Step 1. Ask them to think any Number\n";
+    cout<< "Meanwhile, think of any Number in your head\n";
+    cout<< "Step 2. Ask them to Double the Number they just thought\n";
+    cout<< "Step 3. Ask them to Add the Number in it\n";
+    cout<< "Step 4. Let them Divide it by 2\n";
+    cout<< "Step 5. Ask them to Subtract the Original Number they had thought\n";
+    cout<< "Final Answer should be half of the Number you thought\n";
+    cout<< "\nK Thanks, Bye Bye "<< name;
     cout<< NEWLINE<< NEWLINE<< "Created by Jugal Kishore -- 2019"<< NEWLINE;
     return 0;
 }
