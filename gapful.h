2ed907272bd567ac79e89389d71acc6096538d14
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-01-15 21:18:17 +0530

    Update: gapful.h
    Adding code to check if entered number is three digit number or not.

--- gapful.h
+++ gapful.h
@@ -5,22 +5,44 @@ using namespace std;
 
 int gapful()
 {
-    int a,b,c,d;
+    int a,b,c,d,temp,n;
     cout<< "///Program for Gapful Number///"<< endl<< endl;
     cout<< "Enter Number: ";
     cin>> a;
-    b=a%10;
-    c=a/100;
-    c=c%10;
-    d=c*10+b;
-    if(a%d==0)
+    temp=a;
+    while(temp!=0)
+    {
+        temp=temp/10;
+        n++;
+    }
+    if(n==3)
+    {
+        b=a%10;
+        c=a/100;
+        c=c%10;
+        d=c*10+b;
+        if(a%d==0)
         {
-            cout<< "Entered Number "<< a<< " is a Gapful Number.";
+            cout<< "\nEntered Number "<< a<< " is a Gapful Number.";
         }
+        else
+        {
+            cout<< "\nEntered Number "<< a<< " is not a Gapful Number.";
+        }
+    }
     else
+    {
+        if(n>3)
         {
-            cout<< "Entered Number "<< a<< " is not a Gapful Number.";
+            cout<< "\nGapful Number can't be more than 3 Digits.";
+            cout<< "\nKindly Enter 3 Digits Number.";
         }
+        else
+        {
+            cout<< "\nGapful Number can't be less than 3 Digits.";
+            cout<< "\nKindly Enter 3 Digits Number.";
+        }
+    }
     cout<< NEWLINE<< NEWLINE<< "Created by Jugal Kishore -- 2019"<< NEWLINE;
     return 0;
 }
