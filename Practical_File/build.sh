b950eeb9fa24d7151e8f0e9662a7e2eeff3f382a
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-01-15 22:29:05 +0530

    Fix: build scripts

--- Practical_File/build.sh
+++ Practical_File/build.sh
@@ -1,32 +1,33 @@
-#!/bin/bash
-echo Clearing Object Files and Executable Files
-rm -r *.exe *.o *.out
-echo Done!
+#!/usr/bin/env bash
+
+echo "Clearing Object Files and Executable Files"
+rm -r '*.exe' '*.o' '*.out'
+echo "Done!"
 sleep .5
 clear
-echo Available Files -
-for f in $(ls *.cpp); do echo $f; done
+echo "Available Files -"
+for f in *.cpp; do echo "$f"; done
 echo
-echo Enter File Name -- Without Extension
-read fname
+echo "Enter File Name -- Without Extension"
+read -r fname
 if [[ -f ${fname}.cpp ]]; then
-    echo What do you want to compile it with?
-    echo Clang++ or G++
-    echo Enter '1' for Clang++ and '2' for G++
-    read temp
+    echo "What do you want to compile it with?"
+    echo "Clang++ or G++"
+    echo "Enter '1' for Clang++ and '2' for G++"
+    read -r temp
     COMPILED_PROGRAM=a.out
     if [[ $temp = "1" ]]; then
         clear
-        echo Compiling $fname.cpp with Clang++
-        clang++ $fname.cpp
+        echo "Compiling "$fname.cpp" with Clang++"
+        clang++ "$fname.cpp"
         if [[ -f ${COMPILED_PROGRAM} ]]; then
             echo
-            echo Build With Clang++ without any errors
+            echo "Build With Clang++ without any errors"
             sleep .5
             clear
         else
             echo
-            echo Check the Program Please
+            echo "Check the Program Please"
             sleep 5
             clear
             exit
@@ -34,24 +35,24 @@ if [[ -f ${fname}.cpp ]]; then
     else
         if [[ $temp = "2" ]]; then
             clear
-            echo Compiling $fname.cpp with G++
-            g++ $fname.cpp
+            echo "Compiling "$fname.cpp" with G++"
+            g++ "$fname.cpp"
             if [[ -f ${COMPILED_PROGRAM} ]]; then
                 echo
-                echo Build With G++ without any errors
+                echo "Build With G++ without any errors"
                 sleep .5
                 clear
             else
                 echo
-                echo Check the Program Please
+                echo "Check the Program Please"
                 sleep 5
                 clear
                 exit
             fi
         else
-            if [ $temp != "1" ] && [ $temp != "2" ]; then
-                echo Wrong Compiler
-                echo Exiting!
+            if [ "$temp" != "1" ] && [ "$temp" != "2" ]; then
+                echo "Wrong Compiler"
+                echo "Exiting!"
                 sleep 1
                 clear
                 exit
@@ -59,29 +60,29 @@ if [[ -f ${fname}.cpp ]]; then
         fi
     fi
 else
-    echo File Does not Exist!
-    echo Exiting...
+    echo "File Does not Exist!"
+    echo "Exiting..."
     sleep 1
     clear
     exit
 fi
-echo Running Compiled Program Now
+echo "Running Compiled Program Now"
 sleep 1
 clear
 ./a.out
 echo
-echo Clear Screen?
-echo '1' for yes and anything else for no
-read temp
+echo "Clear Screen?"
+echo "'1' for yes and anything else for no"
+read -r temp
 if [[ $temp = "1" ]]; then
     clear
-    echo K Thanks, Bye!
+    echo "K Thanks, Bye!"
     sleep 3
     clear
     exit
 else
     if [[ $temp != "1" ]]; then
-        echo K Thanks, Bye!
+        echo "K Thanks, Bye!"
         exit
     fi
 fi
--- build_all.sh
+++ build_all.sh
@@ -3,26 +3,26 @@
 echo ""
 echo "Compiling with G++"
 echo ""
-for f in $(ls *.cpp); do echo "Compiling $f"; g++ $f; done
+for f in *.cpp; do echo "Compiling $f"; g++ "$f"; done
 echo ""
 echo "Compiling Practical_File/*"
 echo ""
-for f in $(ls Practical_File/*.cpp); do echo "Compiling $f"; g++ $f; done
+for f in Practical_File/*.cpp; do echo "Compiling $f"; g++ "$f"; done
 echo ""
 echo "Compiling Practical_File/concat/*"
 echo ""
-for f in $(ls Practical_File/concat/*.cpp); do echo "Compiling $f"; g++ $f; done
+for f in Practical_File/concat/*.cpp; do echo "Compiling $f"; g++ "$f"; done
 echo ""
 echo ""
 echo "Compiling with Clang++"
 echo ""
-for f in $(ls *.cpp); do echo "Compiling $f"; clang++ $f; done
+for f in *.cpp; do echo "Compiling $f"; clang++ "$f"; done
 echo ""
 echo "Compiling Practical_File/*"
 echo ""
-for f in $(ls Practical_File/*.cpp); do echo "Compiling $f"; clang++ $f; done
+for f in Practical_File/*.cpp; do echo "Compiling $f"; clang++ "$f"; done
 echo ""
 echo "Compiling Practical_File/concat/*"
 echo ""
-for f in $(ls Practical_File/concat/*.cpp); do echo "Compiling $f"; clang++ $f; done
+for f in Practical_File/concat/*.cpp; do echo "Compiling $f"; clang++ "$f"; done
 echo ""
