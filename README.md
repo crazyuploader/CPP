d790e5a7119faa9383f4e31ff03dda988e77e539
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-01-15 20:41:34 +0530

    Adding: Gapful Number Program

--- README.md
+++ README.md
@@ -93,3 +93,6 @@ Just a collection of my C++ Programs
 
 <b>[`trick.cpp`](trick.cpp)</b>
 * A little math game ;p
+
+<b>[`gapful.cpp`](gapful.cpp)</b>
+* Program to Display if an Entered Number is Gapful or not.
---
+++ gapful.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+#define NEWLINE '\n'
+#define TAB '\t'
+
+int main()
+{
+    int a,b,c,d;
+    cout<< "///Program for Gapful Number///"<< endl<< endl;
+    cout<< "Enter Number: ";
+    cin>> a;
+    b=a%10;
+    c=a/100;
+    c=c%10;
+    d=c*10+b;
+    if(a%d==0)
+        {
+            cout<< "Entered Number "<< a<< " is a Gapful Number.";
+        }
+    else
+        {
+            cout<< "Entered Number "<< a<< " is not a Gapful Number.";
+        }
+    cout<< NEWLINE<< NEWLINE<< "Created by Jugal Kishore -- 2019"<< NEWLINE;
+    return 0;
+}
