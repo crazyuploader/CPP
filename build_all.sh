b57ccde6bae4fe42e349484cb783086e476aa313
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-04-13 21:38:27 +0530

    Update: build_all.sh

--- build_all.sh
+++ build_all.sh
@@ -9,27 +9,26 @@ MAGENTA="\033[1;35m"
 clear
 echo -e "${GREEN}Compiling with G++ ${NC}"
 echo ""
-for f in *.cpp; do echo "Compiling '$f'"; g++ "$f"; done
+for f in *.cpp; do echo "Compiling '${f}'"; g++ "${f}"; done
 echo ""
 echo -e "${YELLOW}" "In Directory Practical_File/* ${NC}"
 echo ""
-for f in Practical_File/*.cpp; do echo "Compiling '$f'"; g++ "$f"; done
+for f in Practical_File/*.cpp; do echo "Compiling '${f}'"; g++ "${f}"; done
 echo ""
 echo -e "${YELLOW}" "In Directory Practical_File/concat/* ${NC}"
 echo ""
-for f in Practical_File/concat/*.cpp; do echo "Compiling '$f'"; g++ "$f"; done
-echo ""
+for f in Practical_File/concat/*.cpp; do echo "Compiling '${f}'"; g++ "${f}"; done
 echo ""
 echo -e "${GREEN}Compiling with Clang++ ${NC}"
 echo ""
-for f in *.cpp; do echo "Compiling '$f'"; clang++ "$f"; done
+for f in *.cpp; do echo "Compiling '${f}'"; clang++ "${f}"; done
 echo ""
 echo -e "${YELLOW}" "In Directory Practical_File/* ${NC}"
 echo ""
-for f in Practical_File/*.cpp; do echo "Compiling '$f'"; clang++ "$f"; done
+for f in Practical_File/*.cpp; do echo "Compiling '${f}'"; clang++ "${f}"; done
 echo ""
 echo -e "${YELLOW}" "In Directory Practical_File/concat/* ${NC}"
 echo ""
-for f in Practical_File/concat/*.cpp; do echo "Compiling '$f'"; clang++ "$f"; done
+for f in Practical_File/concat/*.cpp; do echo "Compiling '${f}'"; clang++ "${f}"; done
 echo ""
 echo -e "${MAGENTA}Done! ${NC}"
