af2b4c9b83cce1789094516e34020aff92bd1866
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-04-13 21:15:11 +0530

    Update: build_all.sh

--- build_all.sh
+++ build_all.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # Colors
-GREEN='\033[1;32m'
-NC='\033[0m'
-YELLOW='\033[1;33m'
-MAGENTA='\033[1;35m'
+GREEN="\033[1;32m"
+NC="\033[0m"
+YELLOW="\033[1;33m"
+MAGENTA="\033[1;35m"
 
-echo ""
-echo -e "${GREEN}" "Compiling with G++ ${NC}"
+clear
+echo -e "${GREEN}Compiling with G++ ${NC}"
 echo ""
 for f in *.cpp; do echo "Compiling '$f'"; g++ "$f"; done
 echo ""
@@ -20,7 +20,7 @@ echo ""
 for f in Practical_File/concat/*.cpp; do echo "Compiling '$f'"; g++ "$f"; done
 echo ""
 echo ""
-echo -e "${GREEN}" "Compiling with Clang++ ${NC}"
+echo -e "${GREEN}Compiling with Clang++ ${NC}"
 echo ""
 for f in *.cpp; do echo "Compiling '$f'"; clang++ "$f"; done
 echo ""
@@ -32,4 +32,4 @@ echo -e "${YELLOW}" "In Directory Practical_File/concat/* ${NC}"
 echo ""
 for f in Practical_File/concat/*.cpp; do echo "Compiling '$f'"; clang++ "$f"; done
 echo ""
-echo -e "${MAGENTA}" "Done! ${NC}"
+echo -e "${MAGENTA}Done! ${NC}"
