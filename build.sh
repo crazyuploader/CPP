f34036bdd54a15e09bccbd6a7671a26aa6f83fb3
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-04-13 21:31:59 +0530

    Update build script
    Update:
    Practical_File/build.sh
    Practical_File/concat/build.sh
    build.sh

--- build.sh
+++ build.sh
@@ -1,31 +1,32 @@
 #!/usr/bin/env bash
 
 # Colors
-GREEN='\033[1;32m'
-NC='\033[0m'
-YELLOW='\033[1;33m'
-MAGENTA='\033[1;35m'
-RED='\033[0;31m'
+GREEN="\033[1;32m"
+NC="\033[0m"
+YELLOW="\033[1;33m"
+MAGENTA="\033[1;35m"
+RED="\033[0;31m"
 
 echo "Clearing Object Files and Executable Files"
-rm -r '*.exe' '*.o' '*.out'
+rm -r "*.exe" "*.o" "*.out"
 echo "Done!"
 sleep .5
 clear
 echo -e "${GREEN}" "Available Files -${NC}"
 for f in *.cpp; do echo "$f"; done
 echo ""
-echo -e "Enter File Name -- ${YELLOW}Without Extension${NC}"
+echo -e "Enter File Name -- ${YELLOW}Without '.cpp' Extension${NC}"
 read -r fname
 if [[ -f ${fname}.cpp ]]; then
-    echo "What do you want to compile it with?"
+    echo ""
+    echo "What do you want to compile '${fname}.cpp' with?"
     echo "Clang++ or G++"
     echo "Enter '1' for Clang++ and '2' for G++"
     read -r temp
     COMPILED_PROGRAM=a.out
-    if [[ $temp = "1" ]]; then
+    if [[ ${temp} = "1" ]]; then
         clear
-        echo "Compiling ""$fname.cpp"" with Clang++"
+        echo "Compiling '$fname.cpp' with Clang++"
         clang++ "$fname.cpp"
         if [[ -f ${COMPILED_PROGRAM} ]]; then
             echo ""
@@ -40,10 +41,10 @@ if [[ -f ${fname}.cpp ]]; then
             exit
         fi
     else
-        if [[ $temp = "2" ]]; then
+        if [[ ${temp} = "2" ]]; then
             clear
-            echo "Compiling ""$fname.cpp"" with G++"
-            g++ "$fname.cpp"
+            echo "Compiling '${fname}.cpp' with G++"
+            g++ "${fname}.cpp"
             if [[ -f ${COMPILED_PROGRAM} ]]; then
                 echo ""
                 echo "Build With G++ without any errors"
@@ -57,8 +58,8 @@ if [[ -f ${fname}.cpp ]]; then
                 exit
             fi
         else
-            if [ "$temp" != "1" ] && [ "$temp" != "2" ]; then
-                echo -e "${RED}" "Wrong Compiler${NC}"
+            if [ "${temp}" != "1" ] && [ "${temp}" != "2" ]; then
+                echo -e "${RED}Wrong Compiler${NC}"
                 echo "Exiting!"
                 sleep 1
                 clear
@@ -67,9 +68,10 @@ if [[ -f ${fname}.cpp ]]; then
         fi
     fi
 else
-    echo -e "${RED}" "File Does not Exist!${NC}"
+    echo ""
+    echo -e "${RED}File Does not Exist!${NC}"
     echo "Exiting..."
-    echo -e "${MAGENTA}" "K Thanks, Bye!${NC}"
+    echo -e "${MAGENTA}K Thanks, Bye!${NC}"
     sleep 1
     clear
     exit
@@ -79,28 +81,30 @@ sleep 1
 clear
 ./a.out
 echo ""
-echo -e "${YELLOW}" "Show Source Code?${NC}"
+echo -e "${YELLOW}Show Source Code?${NC}"
 echo ""
 echo "'y' for yes and anything else for no"
 read -r temp
-if [[ $temp = "y" ]]; then
+if [[ ${temp} = "y" ]]; then
     clear
+    echo -e "${YELLOW}///// Source Code for '${fname}'.cpp /////${NC}"
+    echo ""
     cat "${fname}.cpp"
     sleep 5
     echo ""
     echo "Press 'y' to Exit"
     read -r temp
-    if [[ $temp = "y" ]]; then
+    if [[ ${temp} = "y" ]]; then
         clear
-        sleep 3
-        echo -e "${MAGENTA}" "K Thanks, Bye!${NC}"
+        sleep 1
+        echo -e "${MAGENTA}K Thanks, Bye!${NC}"
         exit
     fi
 else
-    if [[ $temp != "y" ]]; then
+    if [[ ${temp} != "y" ]]; then
         clear
-        echo -e "${MAGENTA}" "K Thanks, Bye!${NC}"
-        sleep 2
+        echo -e "${MAGENTA}K Thanks, Bye!${NC}"
+        sleep 1
         exit
     fi
 fi
